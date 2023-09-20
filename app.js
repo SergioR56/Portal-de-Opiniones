87fece80983e76d14cb378c31107ea079eaa74eb
ChicheSabalero <chiche.sparrow@email.com>
2023-09-20 13:17:05 +0200

    errores

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const {
+    notFoundController,
+    errorController,
+} = require('./src/controllers/errors');
+
 
 const app = express();
+app.use(cors());
+app.use(morgan('dev'));
 
+
+
+app.use(notFoundController);
+app.use(errorController);
 
 
 
