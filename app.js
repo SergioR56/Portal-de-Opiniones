e7da5011e00c23bb798e2312d3809829c06ed5f6
Sergio Ramos <sergi24c@gmail.com>
2023-09-20 13:22:44 +0200

    Middleware parseo del body

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(fileUpload());
 
 app.use(cors());
 
