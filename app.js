f83077ac321125d4e5828527429d1ec1169c1374
JulioRodnu <138291700+JulioRodnu@users.noreply.github.com>
2023-09-20 13:19:06 +0200

    middlewares

--- app.js
+++ app.js
@@ -3,11 +3,56 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser'); 
 
+const { check, validationResult } = require('express-validator'); 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('Bienvenido a la web de reseñas');
+});
+
+app.post('/users', [
+
+  check('mail').isEmail().normalizeEmail(),
+  check('contraseña').isLength({ min: 6 }),
+], (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  res.json({ message: 'Usuario creado' });
+});
+
+app.post('/reseñas', [
+
+  check('texto').notEmpty(),
+], (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  res.json({ message: 'Reseña publicada' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.httpStatus || 500).json({
+    status: 'error',
+    message: err.message,
+  });
+});
 
 
 
