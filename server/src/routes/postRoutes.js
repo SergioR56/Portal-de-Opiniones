d049dbd6c0903cf375769fca58cd92ca01fe0a4d
ChicheSabalero <chiche.sparrow@email.com>
2023-09-23 16:01:34 +0200

    Correccion-Posts

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,4 +1,36 @@
+// const express = require('express');
+// const router = express.Router();
+
+
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
+const {
+    authUserController,
+    authUserOptionalController,
+    postExistsController
+} = require('../middlewares/authUserController');
+
+const {
+    newPostController,
+    listPostsController,
+} = require('../controllers/posts');
+
+
+router.post('/posts', authUserController, newPostController);
+
+router.get('/posts', authUserOptionalController, listPostsController);
+
+
+
+router.delete(
+    '/posts/:postId',
+    authUserController,
+    postExistsController
+);
+
 
 module.exports = router;
+
---
+++ server/src/utils/validateSchema.js
@@ -0,0 +1,10 @@
+const validateSchema = async (schema, data) => {
+    try {
+        await schema.validateAsync(data);
+    } catch (err) {
+        err.httpStatus = 400;
+        throw err;
+    }
+};
+
+module.exports = validateSchema;
